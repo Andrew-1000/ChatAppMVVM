80067e994c92158e68df8d3a4264d35f661fdca0
Andrew-1000 <andruambia95@gmail.com>
2020-05-23 18:16:37 +0300

    Authentication with anonymous credentials enabled | Observers created to monitor on the state of Authentication

--- app/src/main/java/com/example/chatappmvvm/viewmodel/LoginViewModel.java
+++ app/src/main/java/com/example/chatappmvvm/viewmodel/LoginViewModel.java
@@ -1,4 +1,88 @@
 package com.example.chatappmvvm.viewmodel;
 
-public class LoginViewModel {
+import androidx.annotation.NonNull;
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+import com.example.chatappmvvm.BR;
+import com.example.chatappmvvm.utils.MyUtils;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+import java.util.Observer;
+
+public class LoginViewModel extends BaseObservable {
+    public boolean isAuthDone;
+    private boolean isAuthInProgress;
+    public ArrayList<Observer> observers;
+
+
+    public LoginViewModel() {
+        observers = new ArrayList<>(  );
+    }
+
+    @Bindable
+
+    public boolean isAuthDone() {
+        return isAuthDone;
+    }
+    public void setAuthDone(boolean authDone) {
+        isAuthDone = authDone;
+        notifyPropertyChanged( BR.authDone );
+    }
+
+    @Bindable
+    public  boolean isAuthInProgress () {
+        return isAuthInProgress;
+    }
+    public void setAuthInProgress(boolean authInProgress) {
+        isAuthInProgress = authInProgress;
+        notifyPropertyChanged( BR.authInProgress );
+    }
+
+    public void firebaseAnonymousAuth() {
+        setAuthInProgress( true );
+        FirebaseAuth.getInstance().signInAnonymously().addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                setAuthInProgress( false );
+                if (!task.isSuccessful()) {
+                    notifyObservers( MyUtils.SHOW_TOAST, MyUtils.MESSAGE_AUTHENTICATION_FAILED );
+
+                } else {
+                    setAuthDone( true );
+                }
+            }
+        } );
+    }
+
+    public void invalidateRoomName(String roomName) {
+        if (roomName.trim().isEmpty()) {
+            notifyObservers( MyUtils.SHOW_TOAST, MyUtils.MESSAGE_AUTHENTICATION_FAILED );
+
+        } else {
+            notifyObservers( MyUtils.OPEN_ACTIVITY, roomName );
+        }
+    }
+
+    public void addObserver(Observer client) {
+        if (!observers.contains( client )) {
+            observers.add( client );
+        }
+    }
+
+    public void removeObserver(Observer clientToRemove) {
+        if (observers.contains( clientToRemove )) {
+            observers.remove( clientToRemove );
+        }
+    }
+
+    private void notifyObservers(int showToast, String messageAuthenticationFailed) {
+        for (int i = 0; i < observers.size(); i++) {
+            observers.get( i ).onObserve( showToast, messageAuthenticationFailed );
+        }
+    }
 }
