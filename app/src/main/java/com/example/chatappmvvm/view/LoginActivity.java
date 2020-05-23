31745fc22bfe79e0f2cae8b47056c2ab449b7a51
Andrew-1000 <andruambia95@gmail.com>
2020-05-23 18:34:55 +0300

    ActivityLoginBinfing class created to allow for easily binding of UI components using a declarative format

--- app/src/main/java/com/example/chatappmvvm/view/LoginActivity.java
+++ app/src/main/java/com/example/chatappmvvm/view/LoginActivity.java
@@ -1,16 +1,37 @@
 package com.example.chatappmvvm.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.app.Dialog;
 import android.os.Bundle;
 
 import com.example.chatappmvvm.R;
+import com.example.chatappmvvm.databinding.ActivityLoginBinding;
+import com.example.chatappmvvm.viewmodel.LoginViewModel;
 
-public class LoginActivity extends AppCompatActivity {
+import java.util.Observable;
+import java.util.Observer;
 
+public class LoginActivity extends AppCompatActivity implements Observer<String> {
+    private LoginViewModel loginViewModel;
+    private Dialog chatRoomDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_login );
+
+        ActivityLoginBinding activityLoginBinding = DataBindingUtil.setContentView( this, R.layout.activity_login );
+
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
