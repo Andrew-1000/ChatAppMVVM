e255227aa1a0e7e8dcfe717ee664affd3cb736b4
Andrew-1000 <andruambia95@gmail.com>
2020-05-23 17:47:47 +0300

    LoginViewModel Class created | xml view of LoginActivity created | added more styling of texts and buttons in the styles file

--- app/src/main/java/com/example/chatappmvvm/interfaces/ModelCallBack.java
+++ app/src/main/java/com/example/chatappmvvm/interfaces/ModelCallBack.java
@@ -1,4 +1,10 @@
 package com.example.chatappmvvm.interfaces;
 
+import com.example.chatappmvvm.models.ChatPojo;
+
+import java.util.ArrayList;
+
 public class ModelCallBack {
+     public void onModelUpdated(ArrayList<ChatPojo> messages) {
+    }
 }
---
+++ app/src/main/java/com/example/chatappmvvm/viewmodel/LoginViewModel.java
@@ -0,0 +1,4 @@
+package com.example.chatappmvvm.viewmodel;
+
+public class LoginViewModel {
+}
