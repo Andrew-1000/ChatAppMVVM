981bbb0a5ed44f599476f2c06400308443bd9d8c
Andrew-1000 <andruambia95@gmail.com>
2020-05-28 21:12:44 +0300

    Contains observer to check for any update of messages through the Model class ChatPojo

--- app/src/main/java/com/example/chatappmvvm/view/ChatActivity.java
+++ app/src/main/java/com/example/chatappmvvm/view/ChatActivity.java
@@ -2,10 +2,13 @@ package com.example.chatappmvvm.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 
 import com.example.chatappmvvm.R;
+import com.example.chatappmvvm.adapter.ChatAdapter;
+import com.example.chatappmvvm.databinding.ActivityChatBinding;
 import com.example.chatappmvvm.interfaces.Observer;
 import com.example.chatappmvvm.models.ChatPojo;
 import com.example.chatappmvvm.utils.MyUtils;
@@ -25,8 +28,28 @@ public class ChatActivity extends AppCompatActivity implements Observer<ArrayLis
 
         activityChatBinding = DataBindingUtil.setContentView( this, R.layout.activity_chat );
         chatViewModel = new ChatViewModel(getIntent().getStringExtra( MyUtils.EXTRA_ROOM_NAME ));
+        activityChatBinding.setViewModel( chatViewModel );
+        activityChatBinding.setActivity( this );
+        activityChatBinding.recyclerView.setLayoutManager( new LinearLayoutManager( this ) );
+        chatViewModel.addObserver( this );
+        chatViewModel.setListener();
+    }
+    public void sendMessage() {
+        chatViewModel.sendMessageToFirebase( activityChatBinding.edittextChatMessage.getText().toString() );
+        activityChatBinding.edittextChatMessage.getText().clear();
+    }
 
+    @Override
+    public void onObserve(int event, ArrayList<ChatPojo> eventMessage) {
+        ChatAdapter chatAdapter = new ChatAdapter(this, eventMessage);
+        activityChatBinding.recyclerView.setAdapter( chatAdapter );
+        activityChatBinding.recyclerView.scrollToPosition( eventMessage.size()-1 );
+    }
 
-
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        chatViewModel.removeObserver( this );
+        chatViewModel.onDestroy();
     }
 }
